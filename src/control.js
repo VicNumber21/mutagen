e9af04a2fa0fe7c929b43ec25833c769a8477873
Victor Portnov <vic.portnov@gmail.com>
2015-09-15 23:21:03 +0300

    Move run from Runner to Control

--- src/control.js
+++ src/control.js
@@ -19,6 +19,24 @@ var Control = {
     return function () {
       return mutator(gen());
     };
+  },
+
+  run: function (generator, mutators, appender, target) {
+    if (typeof appender !== 'undefined') {
+      var targetUnknown = (typeof target === 'undefined');
+      mutators.push(targetUnknown? appender: appender(target));
+    }
+
+    var mutator = Control.compose(mutators);
+    var mutant = Control.mutate(generator, mutator);
+
+    var result = target;
+    for (var it = mutant(); it !== Control.End; it = mutant()) {
+      if (it !== Control.Skip)
+        result = it;
+    }
+
+    return result;
   }
 };
 
--- src/runner.js
+++ src/runner.js
@@ -5,35 +5,17 @@ var Runner = function (gen) {
   this._mutators = [];
 };
 
-Runner.prototype.run =  function (appender, target) {
-  if (typeof appender !== 'undefined') {
-    var targetUnknown = (typeof target === 'undefined');
-    this._mutators.push(targetUnknown? appender: appender(target));
-  }
-
-  var mutator = Control.compose(this._mutators);
-  var mutant = Control.mutate(this._gen, mutator);
-
-  var result = target;
-  for (var it = mutant(); it !== Control.End; it = mutant()) {
-    if (it !== Control.Skip)
-      result = it;
-  }
-
-  return result;
-};
-
 Runner.addMutator = function (name, mutator, options) {
   Runner.prototype[name] = function () {
       this._mutators.push(mutator.apply(null, arguments));
 
-      return options.final? this.run(): this;
+      return options.final? Control.run(this._gen, this._mutators): this;
     };
 };
 
 Runner.addAppender = function (name, appender, options) {
   Runner.prototype[name] = function (target) {
-    return this.run(appender, target || options.empty());
+    return Control.run(this._gen, this._mutators, appender, target || options.empty());
   };
 };
 
